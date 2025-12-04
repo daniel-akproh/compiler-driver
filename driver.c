da3419fcb9dbfeb3898f9f55138d25d5e6a32758
Daniel Akproh <daniel.akproh@outlook.com>
2025-12-03 23:08:22 -0500

    build and execute pre-processing command

--- driver.c
+++ driver.c
@@ -1,25 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
-    // command to pass gcc -E -P driver.c -o driver.i
-    if (argc != 3) {
-        printf("two args required!\n");
+    char command[50];
+    snprintf(command, sizeof(command), "gcc -E -P %s.c -o %s.i", argv[1], argv[2]);
+
+    if(system(command) != 0) {
+        printf("something went wrong...");
         exit(-1);
     }
-
-    int lower = atoi(argv[1]);
-    int higher = atoi(argv[2]);
-
-    for (int i = lower; i <= higher; i++) {
-        printf("%d\n", i);
-    }
-    
-    // printf("argc: %d\n", argc);
-
-    // for (int i = 0; i < argc; i++) {
-    //     printf("argv[%d]=%s\n", i, argv[i]);
-    // }
 
     return 0;
 }
